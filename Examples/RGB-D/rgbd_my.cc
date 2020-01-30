81a6f11dde9487d63930ac62720be0a0e78c88ef
slaming <liuzhenbo1@mail.nwpu.edu.cn>
2020-01-29 22:26:09 -0500

    add readme

--- Examples/RGB-D/rgbd_my.cc
+++ Examples/RGB-D/rgbd_my.cc
@@ -18,13 +18,12 @@
 * along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <chrono>
 
-#include<iostream>
-#include<algorithm>
-#include<fstream>
-#include<chrono>
-
-#include<opencv2/core/core.hpp>
+#include <opencv2/core/core.hpp>
 
 #include <System.h>
 #include <boost/format.hpp>
@@ -36,41 +35,38 @@ void LoadImages(const string &strAssociationFilename, vector<string> &vstrImageF
 
 int main(int argc, char **argv)
 {
-    if(argc != 6)
+    if (argc != 6)
     {
-        cerr << endl << "Usage: ./rgbd_tum path_to_vocabulary path_to_settings path_to_sequence start_index end_index" << endl;
+        cerr << endl
+             << "Usage: ./rgbd_tum path_to_vocabulary path_to_settings path_to_sequence start_index end_index" << endl;
         return 1;
     }
 
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
-    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::RGBD,true);
+    ORB_SLAM2::System SLAM(argv[1], argv[2], ORB_SLAM2::System::RGBD, true);
 
-
-    int     start_index = atoi( argv[4] );
-    int     end_index = atoi( argv[5] );
-    cout << endl << "-------" << endl;
+    int start_index = atoi(argv[4]);
+    int end_index = atoi(argv[5]);
+    cout << endl
+         << "-------" << endl;
     cout << "Start processing sequence ..." << endl;
 
     // Main loop
     cv::Mat imRGB, imD;
-    for ( int index = start_index; index < end_index; index ++ )
+    for (int index = start_index; index < end_index; index++)
     {
-        boost::format fmt ("%s/rgb_index/%d.png");
+        boost::format fmt("%s/rgb_index/%d.png");
 
-        imRGB = cv::imread( (fmt%argv[3]%index).str(), CV_LOAD_IMAGE_UNCHANGED);
+        imRGB = cv::imread((fmt % argv[3] % index).str(), CV_LOAD_IMAGE_UNCHANGED);
         fmt = boost::format("%s/dep_index/%d.png");
 
-        imD = cv::imread( (fmt%argv[3]%index).str(), CV_LOAD_IMAGE_UNCHANGED);
-        SLAM.TrackRGBD( imRGB, imD, index  );
-
-        ;
+        imD = cv::imread((fmt % argv[3] % index).str(), CV_LOAD_IMAGE_UNCHANGED);
+        SLAM.TrackRGBD(imRGB, imD, index);
     }
     // Stop all threads
     cv::waitKey(0);
 
     SLAM.Shutdown();
 
-
     return 0;
 }
-
--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 ## orbslam2-dense-point-cloud
 
-在[ORBSLAM2](https://github.com/raulmur/ORB_SLAM2)框架基础上，为其添加完整的稠密建图模块。
+在[ORBSLAM2](https://github.com/raulmur/ORB_SLAM2)框架基础上，为其RGBD模式添加完整的稠密建图模块。
 
 
-## 开发日志
+## 记录
+2020年1月29：将高博的ORBSLAM2_with_pointcloud_map集成到自己的工程中。
 
-2020年1月29：将高博的ORBSLAM2_with_pointcloud_map集成到自己的工程中，成功运行。
+[视频](https://www.bilibili.com/video/av85643258?from=search&seid=9965619809264116582)
 
+但是在跑TUM数据集rgbd_dataset_freiburg1_desk过程中，有时候会出现跟踪丢失的情况，所以下一步想要改进一下orbslam2的跟踪性能，提高跟踪速度。
 
-
-
-
+并且增加地图的保存功能
 
 
 
